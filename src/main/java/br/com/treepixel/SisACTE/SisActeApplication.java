dc733e32e14f20a8f6913ebd5a84f00127df5b7f
Antônio Rodrigues <antonio.rodrigues_tga@hotmail.com>
2019-06-03 14:12:56 -0300

    initial skeleton

---
+++ src/main/java/br/com/treepixel/SisACTE/SisActeApplication.java
@@ -0,0 +1,13 @@
+package br.com.treepixel.SisACTE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisActeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisActeApplication.class, args);
+	}
+
+}
